69911b599695be47b74d6657aeaf1b921ebe6898
Chloe CARAYON <chloe.carayon@efrei.net>
2022-02-09 17:31:35 +0100

    feat: add Jenkinsfile and docker compose

---
+++ Jenkinsfile
@@ -0,0 +1,24 @@
+pipeline {
+  agent any
+  stages {
+    stage('Environment') {
+      steps {
+        sh 'git --version'
+        sh 'docker -v'
+        sh 'ls'
+      }
+    }
+    stage('Build Docker test'){
+      steps {
+        //sh 'docker build -t app-jenkins -f Dockerfile --no-cache .'
+         echo 'Here must build'
+      }
+      }
+    stage('Deploy'){
+      steps {
+        echo 'Here must deploy'
+      }
+
+    }
+  }
+}
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,8 @@
 import './App.css';
-import React, {useEffect} from "react";
+import React from "react";
 import CounterApp from './components/counter-app.js';
-import axios from "axios";
-const baseURL ='http://localhost:8083'
-
 
 function App() {
-    useEffect(() => {
-        if (performance.navigation.type === 1) {
-            console.log("This page is reloaded");
-            //CounterApp.setState({reload: true})
-        } else {
-            console.log("This page is not reloaded");
-        }
-    });
-
-    function updateCounter() {
-        axios.get(baseURL)
-            .then(response => this.setState({ counter_time: response.data.pageCount }))
-            .catch(error => {
-                this.setState({ error: error.message });
-                console.error('There was an error!', error);
-            });
-    }
-
   return (
     <div className="App">
       <header className="App-header">
@@ -35,4 +14,3 @@ function App() {
 }
 
 export default App;
-
